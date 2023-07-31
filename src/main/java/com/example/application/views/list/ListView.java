2434a97561e3fa99378ab068e95c80a1a305cfc3
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-07-31 15:09:21 +0200

    Saving work

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,6 +1,10 @@
 package com.example.application.views.list;
 
+import java.util.Collections;
+
 import com.example.application.data.entity.Contact;
+import com.example.application.data.service.CrmService;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -16,12 +20,31 @@ import com.vaadin.flow.router.Route;
 public class ListView extends VerticalLayout {
     Grid<Contact> grid = new Grid<>(Contact.class);
     TextField filterText = new TextField();
+    ContactForm form;
+    CrmService service;
     
-    public ListView() {
+    public ListView(CrmService service) {
+        this.service = service;
         addClassName("list-view");
         setSizeFull();
         configureGrid();
-        add(getToolbar(), grid);
+        configureForm();
+        add(getToolbar(), getContent());
+        updateList();
+    }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid, form);
+        content.setFlexGrow(2, grid);
+        content.setFlexGrow(1, form);
+        content.addClassNames("content");
+        content.setSizeFull();
+        return content;
+    }
+
+    private void configureForm() {
+        form = new ContactForm(Collections.emptyList(), Collections.emptyList());
+        form.setWidth("25em");
     }
 
     private void configureGrid() {
@@ -41,5 +64,9 @@ public class ListView extends VerticalLayout {
         var toolbar = new HorizontalLayout(filterText, addContactButton);
         toolbar.addClassName("toolbar");
         return toolbar;
+    }
+
+    private void updateList() {
+        grid.setItems(service.findAllContacts(filterText.getValue()));
     }
 }
